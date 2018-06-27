a323c58234d26436c1336c7bee6ab5b0d62e770a
serskine <stuart.marr.erskine@gmail.com>
2018-06-27 17:01:47 -0300

    mapped BACKGROUND

--- src/main/java/com/soupthatisthick/dnd/utilities/server/client/model/lookup/Background.java
+++ src/main/java/com/soupthatisthick/dnd/utilities/server/client/model/lookup/Background.java
@@ -1,15 +1,49 @@
 package com.soupthatisthick.dnd.utilities.server.client.model.lookup;
 
 import com.soupthatisthick.dnd.utilities.server.client.model.DaoModel;
+import uk.co.jemos.podam.common.PodamExclude;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Created by Owner on 5/5/2017.
  * Copyright of Stuart Marr Erskine, all rights reserved.
  */
-
+@Entity
+@Table(name = "BACKGROUND")
 public class Background extends DaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sg")
+    @Column(name = "id", updatable = false, nullable = false)
+    @PodamExclude
     private Long id;
-    private String name, skills, tools, languages, equipment, description, features;
+    
+    @Column(name="name")
+    private String name;
+
+    @Column(name="skills")
+    private String skills;
+
+    @Column(name="tools")
+    private String tools;
+
+    @Column(name="languages")
+    private String languages;
+
+    @Column(name="equipment")
+    private String equipment;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="features")
+    private String features;
 
     public Long getId() {
         return id;
